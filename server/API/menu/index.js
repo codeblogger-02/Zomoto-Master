1af769290b444f9b9262c32070488c69c2d3ea77
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-07-30 15:56:43 +0530

    menu router added

---
+++ server/API/menu/index.js
@@ -0,0 +1,49 @@
+//libraries
+import express from "express";
+import passport from "passport";
+
+//database model
+import { MenuModel , ImageModel} from "../../database/allModels" 
+
+const Router = express.Router();
+
+/*
+route      /list
+des         get all menu based on id 
+params      _id
+Access      public
+method      Post
+*/
+
+
+Router.get("/list/:_id", async (req,res) => {
+    try {
+        const {_id} = req.params;
+        const menus = await MenuModel.findOne(_id);
+
+        return res.json({ menus })
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+});
+
+
+/*
+route      /list
+des         get all menu images based on id 
+params      _id
+Access      public
+method      Post
+*/
+
+Router.get("/image/:_id", async (req , res) => {
+    try {
+        const {_id} = req.params;
+        const menus = await ImageModel.findOne(_id);
+
+        return res.json({ menus })
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+});
+
