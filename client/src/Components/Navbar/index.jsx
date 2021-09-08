c7abe12c760d21d203c6632545e27cceb0523dda
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-09-08 20:40:10 +0530

    added navbar for all devices

--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {FaUserAlt} from "react-icons/fa"
+import {HiLocationMarker} from "react-icons/hi"
+import {IoMdArrowDropdown,IoMdArrowDropup} from "react-icons/io"
+import {RiSearchLine} from "react-icons/ri"
 
- const Navbar = () => {
-    return (
-        <>
-           <nav className="p-4 flex bg-white shadow-md w-full items-center justify-between">
-               <div className="w-28">
+const MobileNav = () => {
+     return (
+       <div className="flex w-full items-center justify-between md:hidden">
+         <div className="w-28">
                    <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />     
               </div>
 
@@ -13,6 +15,55 @@ import {FaUserAlt} from "react-icons/fa"
               <button className="bg-zomato-400 text-white py-2 px-3 rounded-full">Use App</button> 
               <span className="border p-2 border-gray-300 text-zomato-400 rounded-full"><FaUserAlt/></span>       
             </div>  
+       </div>
+     )
+};
+
+const MediumNav = () => {
+
+  return (
+    <>
+    
+    <div className="hidden gap-4 w-full items-center  justify-between  md:flex lg:w-3/4">
+    <div className="w-28">
+    <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />     
+    </div>
+
+     <div className="w-full bg-white shadow-md  p-3 flex items-center gap-3 border border-gray-200 rounded">
+       <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
+         <span className="text-zomato-400"><HiLocationMarker/></span>
+         <input type="text" placeholder="Ballarpur" className="focus:outline-none" />
+         <IoMdArrowDropdown />
+       </div>
+       <div className="flex w-full items-center gap-2">
+         <RiSearchLine/>
+         <input type="text" placeholder="Search for Restaurant, cuisine or a dish" className="w-full focus:outline-none"/>
+       </div>
+     </div>
+  
+     <div className="flex gap-2  lg:justify-end">
+       <button className="text-gray-500 text-xl hover:text-gray-800">Login</button>
+       <button className="text-gray-500 text-xl hover:text-gray-800" >SignUp</button>
+     </div>
+
+    </div>
+    
+    
+    </>
+
+  )
+
+
+};
+
+
+ const Navbar = () => {
+    return (
+        <>
+           <nav className="p-4 flex bg-white shadow-md w-full items-center">
+           <MobileNav/>
+           <MediumNav/>
+          
           </nav> 
         </>
     )
