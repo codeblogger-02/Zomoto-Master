0f8c6632581e20b29e91e2ca6de682286b6f55c7
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-09-09 12:02:22 +0530

    fix navbar width issue

--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -6,7 +6,7 @@ import {RiSearchLine} from "react-icons/ri"
 
 const MobileNav = () => {
      return (
-       <div className="flex w-full items-center justify-between md:hidden">
+       <div className="flex w-full items-center justify-between lg:hidden">
          <div className="w-28">
                    <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />     
               </div>
@@ -19,17 +19,17 @@ const MobileNav = () => {
      )
 };
 
-const MediumNav = () => {
+const LargeNav = () => {
 
   return (
     <>
-    
-    <div className="hidden gap-4 w-full items-center  justify-between  md:flex lg:w-3/4">
+    <div className="container px-40 mx-auto">
+    <div className="hidden gap-4 w-full items-center  justify-around  lg:flex ">
     <div className="w-28">
     <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />     
     </div>
 
-     <div className="w-full bg-white shadow-md  p-3 flex items-center gap-3 border border-gray-200 rounded">
+     <div className="w-3/4 bg-white shadow-md  p-3 flex items-center gap-3 border border-gray-200 rounded">
        <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
          <span className="text-zomato-400"><HiLocationMarker/></span>
          <input type="text" placeholder="Ballarpur" className="focus:outline-none" />
@@ -41,14 +41,14 @@ const MediumNav = () => {
        </div>
      </div>
   
-     <div className="flex gap-2  lg:justify-end">
+     <div className="ml-20 flex gap-4 ">
        <button className="text-gray-500 text-xl hover:text-gray-800">Login</button>
        <button className="text-gray-500 text-xl hover:text-gray-800" >SignUp</button>
      </div>
 
     </div>
     
-    
+    </div>
     </>
 
   )
@@ -62,7 +62,7 @@ const MediumNav = () => {
         <>
            <nav className="p-4 flex bg-white shadow-md w-full items-center">
            <MobileNav/>
-           <MediumNav/>
+           <LargeNav/>
           
           </nav> 
         </>
