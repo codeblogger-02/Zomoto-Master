8eeea0c0e7018f2e3d270af2d4d356e4d999e5b7
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-09-09 20:54:06 +0530

    added active tab for mobile screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,25 @@ import HomeLayoutHOC from "./HOC/Home.Hoc";
 
 //Component
 import Temp from "./Components/temp";
+import Master from "./Components/master";
+
 
 
 function App() {
   return (
     <>
     <HomeLayoutHOC  path="/"  exact component={Temp}  />
-    
+    <HomeLayoutHOC  path="/:type"  exact component={Master}  />
     </>
   );
 }
 
 export default App;
+
+
+//delivery
+//Dinning
+//night
+//nutri 
+
+//master -> url parameter
---
+++ client/src/Components/master.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+
+ const Master = () => {
+  
+    const {type} = useParams()
+
+    return (
+        <>
+         {type}   
+        </>
+    )
+};
+
+export default  Master;
