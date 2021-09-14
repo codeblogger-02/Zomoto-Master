920ad5739726724f0e709106af372a58c3f97ad7
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-09-14 10:40:25 +0530

    added restaurant grid

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,36 +1,22 @@
 import React from 'react'
 
+
 //component
-import RestaurantNavbar from "../Components/Navbar/restaurantNavbar"
+import RestaurantNavbar from "../Components/Navbar/restaurantNavbar";
+import ImageGrid from '../Components/restaurant/imageGrid';
+
 
 const RestaurantLayout = () => {
     return (
        <>
          <RestaurantNavbar />
         <div className="container mx-auto px-4 lg:px-40">
-           
-            <div className="w-full h-60  md:hidden">
-                <img src="https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg" alt="restaurant image"
-                className="w-full h-full object-cover rounded-lg"/>
-            </div>
-            <div className="hidden w-full h-80 md:flex gap-1">
-           <div className="w-7/12 h-full overflow-hidden ">
-           <img src="https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg" alt="restaurant image"
-                className="w-full h-full object-cover rounded-lg transform transition duration-500 hover:scale-110 "/>
-           </div>
-           <div className="w-1/6 h-full flex flex-col gap-1 ">
-           <img src="https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg" alt="restaurant image"
-                className="w-full h-full object-cover rounded-lg"/>
-                 <img src="https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg" alt="restaurant image"
-                className="w-full h-full object-cover rounded-lg"/>
-           </div>
-           <div className="w-1/6 h-full flex flex-col gap-1 ">
-           <img src="https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg" alt="restaurant image"
-                className="w-full h-full object-cover rounded-lg"/>
-                 <img src="https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg" alt="restaurant image"
-                className="w-full h-full object-cover rounded-lg"/>
-           </div>
-            </div>
+           <ImageGrid images={["https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg",
+           "https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg",
+           "https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg",
+           "https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg",
+           "https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg"
+           ]} /> 
         </div>
        
        </>
