e3d91db26c278ee341241a80a1e99b0594f573f0
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-09-14 13:01:05 +0530

    added layout

---
+++ client/src/Components/restaurant/InfoButtons.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import classnames from "classnames";
+
+const InfoButtons = (props) => {
+    return (
+        <>
+            <button className={classnames("flex items-center gap-3 border border-zomato-400 px-4 py-2 rounded-lg",{
+                  "text-white bg-zomato-400": props.isActive,
+            })}>{props.children}</button>
+        </>
+    )
+}
+
+export default InfoButtons
--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {TiStarOutline} from "react-icons/ti";
+import {RiDirectionLine , RiShareForwardLine} from "react-icons/ri";
+import {BiBookmarkPlus} from "react-icons/bi";
 
 
 //component
 import RestaurantNavbar from "../Components/Navbar/restaurantNavbar";
 import ImageGrid from '../Components/restaurant/imageGrid';
+import InfoButtons from '../Components/restaurant/InfoButtons';
+import RestaurantInfo from '../Components/restaurant/RestaurantInfo';
+
+
 
 
 const RestaurantLayout = () => {
@@ -17,6 +24,25 @@ const RestaurantLayout = () => {
            "https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg",
            "https://b.zmtcdn.com/data/pictures/0/3300300/bedddb08e3eafa541fdec9db26613993.jpg"
            ]} /> 
+   
+         <RestaurantInfo name="The Pizza Project" restaurantRating="3.5" deliveryRating="3.2" cuisine="Pizza, Fast Food, Beverages" address="Dharampeth, Nagpur" />
+      
+      <div className="my-4 flex flex-wrap gap-3">
+          <InfoButtons isActive >
+              <TiStarOutline /> Add Review
+          </InfoButtons>
+          <InfoButtons >
+              <RiDirectionLine /> Direction
+          </InfoButtons>
+          <InfoButtons >
+              <BiBookmarkPlus /> Bookmark
+          </InfoButtons>
+          <InfoButtons >
+              <RiShareForwardLine /> Share
+          </InfoButtons>
+      </div>
+
+
         </div>
        
        </>
