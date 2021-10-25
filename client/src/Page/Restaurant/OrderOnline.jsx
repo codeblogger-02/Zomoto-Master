b36e8583d72c144bb32abcddf5ba8c3ea8a352b3
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-10-25 19:40:52 +0530

    added foodItem card

--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
-
-
+import {AiOutlineCompass} from "react-icons/ai"
+import {BiTimeFive} from "react-icons/bi"
 
 //component 
 import FloatMenuBtn from '../../Components/restaurant/Order-Online/FloatMenuBtn'
+import FoodItem from '../../Components/restaurant/Order-Online/FoodItem';
 import MenuListContainer from "../../Components/restaurant/Order-Online/MenuListContainer";
+
 
 const OrderOnline = () => {
     return (
         <>
-            <div className="w-full">
+            <div className="w-full flex">
                 <aside className="hidden md:flex flex-col gap-3 border-r overflow-y-scroll border-gray-200 h-screen w-1/4">
                     <MenuListContainer />
                     <MenuListContainer />
                 
                 </aside>
-               <div className="w-full md:w-3/4 "></div>
+               <div className="w-full md:w-3/4 ">
+                   <h2 className="text-xl font-semibold">Order Online</h2>
+                   <h4 className="flex items-center gap-2 font-light text-gray-500">
+                       <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
+                   </h4>
+                   
+                   <section>
+                       <FoodItem image="https://b.zmtcdn.com/data/dish_photos/b2b/7d63231526b090e406540ca0a4411b2b.jpg"
+                        price="500"
+                        rating={3} 
+                        description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa ad consequuntur iste voluptatibus autem quisquam sapiente deleniti doloremque, aliquam impedit ea dolore ab. Sint laboriosam ipsum, dicta delectus atque velit?"
+                        title="Peri Peri Paneer" />
+                   </section>
+               </div>
              </div>
              <FloatMenuBtn />
         </>
